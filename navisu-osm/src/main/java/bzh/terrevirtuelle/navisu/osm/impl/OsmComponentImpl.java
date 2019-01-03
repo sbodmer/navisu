16ffdab97094b4ba9f58f857066b3fa0e6c97d8f
sbodmer <sbodmer@lsi-media.ch>
2019-01-03 22:26:04 +0100

    Fixed the layer service call in OsmCompoentImpl Upgraded gradle version to 2.6 (from 1.7)

--- navisu-osm/src/main/java/bzh/terrevirtuelle/navisu/osm/impl/OsmComponentImpl.java
+++ navisu-osm/src/main/java/bzh/terrevirtuelle/navisu/osm/impl/OsmComponentImpl.java
@@ -1,28 +1,31 @@
 package bzh.terrevirtuelle.navisu.osm.impl;
 
-
 import bzh.terrevirtuelle.navisu.app.drivers.instrumentdriver.InstrumentDriver;
+import bzh.terrevirtuelle.navisu.app.guiagent.layers.LayersManagerServices;
 import bzh.terrevirtuelle.navisu.core.view.geoview.worldwind.impl.GeoWorldWindViewImpl;
 import bzh.terrevirtuelle.navisu.osm.OsmComponent;
 import bzh.terrevirtuelle.navisu.osm.OsmComponentServices;
 import gov.nasa.worldwind.WorldWindow;
 import gov.nasa.worldwind.layers.BasicLayerFactory;
+import gov.nasa.worldwind.layers.RenderableLayer;
 import java.io.InputStream;
 import org.capcaval.c3.component.ComponentState;
+import org.capcaval.c3.component.annotation.UsedService;
 import org.osmbuildings.OSMBuildingsLayer;
 
 /**
  * @author sbodmer
  * @date 24/12/2018 12:49
  */
-
-
 public class OsmComponentImpl
         implements OsmComponent, OsmComponentServices, InstrumentDriver, ComponentState {
 
+    @UsedService
+    LayersManagerServices layersManagerServices;
+
     private static final String NAME = "BUILDING";
     private static final String TYPE_0 = "OSM";
-    
+
     @Override
     public boolean canOpen(String type) {
         boolean canOpen = false;
@@ -36,14 +39,14 @@ public class OsmComponentImpl
 
     @Override
     public void on(String... files) {
-        System.out.println("ON:"+files);
+        System.out.println("ON:" + files);
     }
 
     @Override
     public void componentInitiated() {
         // System.out.println("INITED");
         //--- New instance with hard coded default value
-        OSMBuildingsLayer layer = (OSMBuildingsLayer) GeoWorldWindViewImpl.getWW().getModel().getLayers().getLayersByClass(OSMBuildingsLayer.class).get(0);
+        OSMBuildingsLayer layer = (OSMBuildingsLayer) layersManagerServices.getLayer("On-earth layers", "OSMBuildings");
         //--- Fine tune the layer
         layer.setMaxTiles(9);
         layer.setRows(3);
@@ -61,7 +64,7 @@ public class OsmComponentImpl
         // System.out.println("STARTED");
         // layer.addPreBuildingsRenderer(this);
         // layer.addTileListener(this);
-        
+
     }
 
     @Override
@@ -69,7 +72,7 @@ public class OsmComponentImpl
         // System.out.println("STOPPED");
         // layer.removePreBuildingsRenderer(this);
         // layer.removeTileListener(this);
-        
+
     }
 
     @Override
